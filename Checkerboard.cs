b6e0c7c53846b57750043c715f491eb2562c435f
Nicholas Rishel <rishel.nick@gmail.com>
2014-05-16 14:01:49 -0400

    Initial commit of a working checkered background that can quickly be applied to 2D cameras.

---
+++ Checkerboard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkerboard : MonoBehaviour {
+	[SerializeField]float scaleWorld = 0.25f;
+	// Use this for initialization
+	void Start () {
+		// Size the background to the main camera
+		Vector3 bottomLeft = new Vector3 (0.0f, 0.0f, 10.0f);
+		bottomLeft = Camera.main.ViewportToWorldPoint (bottomLeft);
+		Vector3 topRight = new Vector3 (1.0f, 1.0f, 10.0f);
+		topRight = Camera.main.ViewportToWorldPoint (topRight);
+
+		Vector3 Scale = new Vector3 (topRight.x - bottomLeft.x , topRight.y - bottomLeft.y, 1.0f);
+		transform.localScale = Scale;
+		renderer.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+		renderer.material.SetTextureScale("_MainTex", Scale*scaleWorld);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update the UV coordinates to move the checker
+		renderer.material.SetTextureOffset("_MainTex", transform.position*scaleWorld);
+	}
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Add Checker Object under your Main Camera to create a checkered background.
+
+This implementation assumes:
+* The scene is aligned to the default Unity 2D orientation
+* No object exists beyond Z==12 in the world space
